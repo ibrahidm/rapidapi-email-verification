581d961836edac301653d5bc86e9d01e2e2a001b
Danny Ibrahim <thedannyibrahim@gmail.com>
2026-03-30 22:45:44 -0400

    scaffold project

---
+++ nuxt.config.ts
@@ -0,0 +1,10 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  modules: ['@nuxt/ui'],
+  css: ['~/assets/css/main.css'],
+  imports: {
+    autoImport: false,
+  },
+  devtools: { enabled: false },
+});
