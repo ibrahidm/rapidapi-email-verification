bb31a9f8df6fdd3825f339a1059cce14ab38a048
Danny Ibrahim <thedannyibrahim@gmail.com>
2026-03-31 21:39:38 -0400

    add nuxt analytics

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  modules: ['@nuxt/ui'],
+  modules: ['@nuxt/ui', '@vercel/analytics'],
   css: ['~/assets/css/main.css'],
   imports: {
     autoImport: false,
